bb28234493dc08d2445ff397be8cc6ca92e6eefc
stas7070 <smirnovstas9@gmail.com>
2023-01-10 20:40:37 +0300

    chudom  reshena

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASS = "postgres";
+
+
+    public static Connection getConnecctionDataBase() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            if (connection != null) {
+                System.out.println("База данных подключена!");
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        }
+        return connection;
+    }
 }
